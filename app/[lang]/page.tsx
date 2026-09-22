eb834ce040271ea9ce698c2437b67541f9a89474
nreyl <n.reyl@uniandes.edu.co>
2026-09-22 12:05:25 -0500

    Internacionalización

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,15 @@
+import { notFound } from "next/dist/client/components/not-found";
+import { getDictionary, hasLocale } from "./dictionaries";
+
+export default async function Home({ params }: PageProps<"/[lang]">) {
+  const { lang } = await params;
+  if (!hasLocale(lang)) notFound();
+  const dict = await getDictionary(lang);
+
+  return (
+    <main className="flex min-hscreen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">{dict.welcome}</h1>
+      <p className="text-lg">{dict.description}</p>
+    </main>
+  );
+}
